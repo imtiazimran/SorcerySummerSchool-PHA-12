c3728a947c99192b801caed54cb36be8e2700d70
Imran <imtiazimran@outlook.com>
2023-06-19 22:09:52 +0600

    student route problem fixed with support session

--- src/Components/Dashboard/DashboardOverview.jsx
+++ src/Components/Dashboard/DashboardOverview.jsx
@@ -31,7 +31,7 @@ const DashboardOverview = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                             </div>
                             <div className="stat-title">Total Classes</div>
-                            <div className="stat-value text-primary">{classes.length}</div>
+                            <div className="stat-value text-primary">{classes?.length}</div>
                             <div className="stat-desc">21% more than last month</div>
                         </div>
 
@@ -40,7 +40,7 @@ const DashboardOverview = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                             </div>
                             <div className="stat-title">Total Instructors</div>
-                            <div className="stat-value text-secondary">{instructors.length}</div>
+                            <div className="stat-value text-secondary">{instructors?.length}</div>
                             <div className="stat-desc">21% more than last month</div>
                         </div>
 
@@ -96,7 +96,7 @@ const DashboardOverview = () => {
                 </div>
             }
             {
-                user && (userRole =="" || !userRole) &&
+                user &&
                 <div className="stats shadow">
 
                     <div className="stat">
@@ -113,18 +113,18 @@ const DashboardOverview = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                         </div>
                         <div className="stat-title">Total Selected</div>
-                        <div className="stat-value text-secondary">{cart.length}</div>
+                        <div className="stat-value text-secondary">{cart?.length}</div>
                     </div>
 
                     <div className="stat">
                             <div className="stat-figure text-secondary">
                                 <div className="avatar online">
                                     <div className="w-16 rounded-full">
-                                        <img src={user.photoURL} />
+                                        <img src={user?.photoURL} />
                                     </div>
                                 </div>
                             </div>
-                            <div className="stat-value">{user.displayName}</div>
+                            <div className="stat-value">{user?.displayName}</div>
                             <div className="stat-desc text-secondary">Student</div>
                         </div>
 
