8bad928afd45e34a7e4e19841f6f978b5b7c95dd
imtiazimran <imtiazimran@outlook.com>
2023-10-15 22:25:32 +0600

    email functionality added

--- src/Components/Home/EmailForm.jsx
+++ src/Components/Home/EmailForm.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import Title from '../Shared/Title';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const EmailForm = () => {
-  const { control, handleSubmit, formState: { errors } } = useForm();
+  const { control, handleSubmit, formState: { errors }, reset } = useForm();
+  const [isLoading, setIsLoading] = useState(false)
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
+    setIsLoading(true)
     // You can handle form submission logic here. 'data' contains the form values.
-    console.log('Form submitted:', data);
+    try {
+      const response = await axios.post('https://summer-camp-server-weld.vercel.app/send-email', data); // Make an API request to your backend
+      setIsLoading(false)
+      if (response) {
+        Swal.fire({
+          position: 'top-center',
+          icon: 'success',
+          title: 'Email sended to authority',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+      reset()
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error);
+      Swal.fire({
+        position: 'top-center',
+        icon: 'error',
+        title: 'Email sending failed',
+        text: `${error.message}`,
+        showConfirmButton: false,
+        timer: 1500
+      })
+      console.error('Email sending failed:', error);
+    }
   };
 
   return (
     <div className="bg-white rounded-lg shadow-lg">
-    <Title title={"Contact Us"} subtitle={"Send Direct Email to us for your urgent query "} />
-      <form className='w-1/3 mx-auto flex flex-col justify-center py-10 px-5 bg-slate-100 ' onSubmit={handleSubmit(onSubmit)}>
+      <Title title={"Contact Us"} subtitle={"Send Direct Email to us for your urgent query "} />
+      <form className='md:w-1/3 mx-auto flex flex-col justify-center py-10 px-5 bg-slate-100 ' onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
           <Controller
@@ -68,8 +97,9 @@ const EmailForm = () => {
         </div>
 
         <button
+          disabled={isLoading}
           type="submit"
-          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg"
+          className="btn bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg"
         >
           Submit
         </button>
